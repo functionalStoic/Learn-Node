8b211d6350aac67b285ccb15acec5d7ec171c253
JasonSooter <jason.sooter@gmail.com>
2018-07-04 19:19:38 -0500

    Removed .gitkeep files. Updated 404 error handler

--- handlers/errorHandlers.js
+++ handlers/errorHandlers.js
@@ -15,7 +15,7 @@ exports.catchErrors = fn => {
   Not Found Error Handler
   If we hit a route that is not found, we mark it as 404 and pass it along to the next error handler to display
 */
-exports.notFound = (req, res, next) => {
+exports.notFound = (req, res) => {
   const err = new Error('Not Found');
   err.status = 404;
   err.stack = err.stack || '';
@@ -26,11 +26,7 @@ exports.notFound = (req, res, next) => {
     html: function() {
       res.render('error', {
         message: err.message,
-        status: err.status,
-        stackHighlighted: err.stack.replace(
-          /[a-z_-\d]+.js:\d+:\d+/gi,
-          '<mark>$&</mark>'
-        )
+        status: err.status
       });
     },
     json: function() {
@@ -62,7 +58,6 @@ exports.flashValidationErrors = (err, req, res, next) => {
   In development we show good error messages so if we hit a syntax error or any other previously un-handled error, we can show good info on what happened
 */
 exports.developmentErrors = (err, req, res /* , next */) => {
-  console.log('Got to developmentErrors');
   err.stack = err.stack || '';
   const errorDetails = {
     message: err.message,
